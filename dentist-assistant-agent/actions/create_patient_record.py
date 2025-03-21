45ce8f8c882169c7b87862de342a4a7f2e6746e2
Muhammed Emin Tetik <m.e.tetik03@gmail.com>
2025-03-21 20:29:57 +0000

    dentist-assistant-agent rasa project created. create_patient_record flow created

---
+++ dentist-assistant-agent/actions/create_patient_record.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict, List, Text
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+from actions.patient_services import add_patient, get_patients, Patient 
+
+class CreatePatientRecord(Action):
+    def name(self) -> str:
+        return "create_patient_record"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> List[Dict[Text, Any]]:
+        patients = get_patients() # TODO: Sender Id?
+        patient_identity_number = tracker.get_slot("add_patient_identity_number")
+        patient_first_name = tracker.get_slot("add_patient_first_name")
+        patient_last_name = tracker.get_slot("add_patient_last_name")
+        patient_age = tracker.get_slot("add_patient_age")
+
+        if patient_identity_number is None:
+            return [SlotSet("return_value", "data_not_present")]
+
+        existing_idn = {c["identity_number"] for c in patients}
+        if patient_identity_number in existing_idn:
+            return [SlotSet("return_value", "already_exists")]
+
+        new_patient = Patient(identity_number=patient_identity_number,
+            first_name=patient_first_name,
+            last_name=patient_last_name,
+            age=patient_age)
+        
+        result = add_patient(new_patient)
+        if result:
+            return [SlotSet("return_value", "success")]
+        else:
+            return [SlotSet("return_value", "failed")]
